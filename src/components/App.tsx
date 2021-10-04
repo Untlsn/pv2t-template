912abeb2fc8251996e9a864837ce6c3287c96c89
Filip Skoczeń <philipskoczen@gmail.com>
2021-10-04 11:20:26 +0200

    Readd

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,26 @@
 import React, { useState } from 'react';
 import Logo from '~/assets/images/logo.png';
-import * as S from './App.style';
 import prevDef from '~/helpers/prevDef';
-import { GlobalStyle } from '~/assets/style/global';
+import '~/assets/style/global.css';
 
 const App = () => {
   const [count, changeCount] = useState(0);
 
   return (
-    <>
-      <GlobalStyle />
-      <S.Wrapper
+    <div className='flex flex-col h-screen items-center text-xl'>
+      <img src={Logo} alt='' className='h-80  animate-spin-slow'/>
+      <p className='m-0'>Hello Vite + Preact Comp!</p>
+      <p className='m-0'>Just write in react, run in preact</p>
+      <p className='m-0'>Hooks work without issues: {count}</p>
+      <button
+        className='border-black bg-transparent text-lg rounded w-32 mt-3
+        cursor-pointer hover:bg-gray-100 transition-colors duration-300'
         onClick={() => changeCount(old => old + 1)}
-        onContextMenu={prevDef(() => changeCount(0))}>
-        <S.Image src={Logo}/>
-        <p>Hello Vite + Preact Comp!</p>
-        <p>Just write in react, run in preact</p>
-        <p>Hooks work without issues: {count}</p>
-      </S.Wrapper>
-    </>
+        onContextMenu={prevDef(() => changeCount(old => old - 1))}
+      >
+        Click
+      </button>
+    </div>
   );
 };
 
